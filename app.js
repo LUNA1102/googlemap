43eae5b237be5643f056a18d6c6e312abd8ce768
unknown <moong3u@naver.com>
2023-06-08 14:04:10 +0900

    commit

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ window.initMap = function () {
         zoom: 10
     });
 
-    new google.maps.Marker({
-        position: { lat: 48.17544626081099, lng: 11.551784725912334 },
-        label: "올림피아파크",
-        map: map
+    const trip = [
+        { label: "A", name: "올림피아파크", lat: 48.175407, lng: 11.551797 },
+        { label: "B", name: "뮌헨 올림픽 스타디움", lat: 48.173111, lng: 11.546587 },
+        { label: "C", name: "님펜부르크 궁전", lat: 48.1582422, lng: 11.5031617 },
+        { label: "D", name: "마리엔 광장", lat: 48.137383, lng: 11.575454 },
+        { label: "E", name: "영국 정원", lat: 48.164182, lng: 11.605574 },
+    ];
+
+    trip.forEach(({ label, lat, lng }) => {
+        new google.maps.Marker({
+            position: { lat, lng },
+            label,
+            map
+        });
     });
 }
