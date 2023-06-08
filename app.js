fd127d114c0209536dd4c1ca196513e830e2a2eb
unknown <moong3u@naver.com>
2023-06-08 12:45:21 +0900

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.initMap = function () {
     new google.maps.Map(document.getElementById("map"), {
         center: { lat: 48.153033, lng: 10.8838333 },
-        zoom: 10
+        zoom: 11
     });
 }
